5f6e63196e1ec85500a0f8d179774e0dfc424197
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-06-16 12:36:43 +0900

    add layout

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>詳細画面</div>
+  <div>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">〇〇のポートフォリオ</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card dark color="secondary">
+            <v-card-text class="px-0"
+              >アバター
+              <v-avatar :tile="false" :size="300" color="grey lighten-4">
+                <img
+                  src="https://avatars0.githubusercontent.com/nsuzuki7713"
+                  alt="avatar"
+                />
+              </v-avatar>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card dark color="secondary">
+            <v-card-text class="px-0">
+              <div>自己紹介</div>
+              <div>自己紹介</div>
+              <div>自己紹介</div>
+              <div>自己紹介</div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">使用技術</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12>
+          <v-card dark color="primary">
+            <v-card-text class="px-0">アピールポイント</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
