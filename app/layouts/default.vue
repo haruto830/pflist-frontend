6cad54b6e2fe638a7fa89f16f5d5aa7112650fdb
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-07-02 23:23:09 +0900

    add header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app dark>
+    <v-toolbar>
+      <v-toolbar-title
+        >pflist
+        <span style="font-size:14px;"
+          >駆け出したいエンジニアに再び光を</span
+        ></v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>ログイン</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
